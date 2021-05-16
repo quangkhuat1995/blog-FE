735a964ac2ce88f9b7dbb72870f3d092056b5f35
quangkhuat <quangkhuat1995@gmail.com>
2021-05-16 13:56:54 +0700

    integrate with user api

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -22,7 +22,11 @@ class Feed extends Component {
   };
 
   componentDidMount() {
-    fetch("URL")
+    fetch("http://localhost:5050/auth/status", {
+      headers: {
+        Authorization: `Bearer ${this.props.token}`,
+      },
+    })
       .then((res) => {
         if (res.status !== 200) {
           throw new Error("Failed to fetch user status.");
@@ -50,7 +54,11 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch("http://localhost:5050/feed/posts?page=" + page)
+    fetch("http://localhost:5050/feed/posts?page=" + page, {
+      headers: {
+        Authorization: `Bearer ${this.props.token}`,
+      },
+    })
       .then((res) => {
         if (res.status !== 200) {
           throw new Error("Failed to fetch posts.");
@@ -74,7 +82,14 @@ class Feed extends Component {
 
   statusUpdateHandler = (event) => {
     event.preventDefault();
-    fetch("URL")
+    fetch("http://localhost:5050/auth/status", {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${this.props.token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ status: this.state.status }),
+    })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error("Can't update status!");
@@ -126,6 +141,9 @@ class Feed extends Component {
     fetch(url, {
       method,
       body: formData,
+      headers: {
+        Authorization: `Bearer ${this.props.token}`,
+      },
     })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
@@ -176,7 +194,12 @@ class Feed extends Component {
 
   deletePostHandler = (postId) => {
     this.setState({ postsLoading: true });
-    fetch("http://localhost:5050/feed/posts/" + postId, { method: "DELETE" })
+    fetch("http://localhost:5050/feed/posts/" + postId, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${this.props.token}`,
+      },
+    })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error("Deleting a post failed!");
